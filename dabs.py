818fae77285ee2e9c69c37554b2f7dffb4dbb6b2
anish <anish.moorthy@gmail.com>
2018-05-16 16:33:51 -0400

    Stuff works

--- dabs.py
+++ dabs.py
@@ -12,7 +12,7 @@ PROTOCOL_VERSION = 1
 COMM_SUCCESS = 0
 COMM_TX_FAIL = -1001
 
-dxl_ids = [12]
+dxl_ids = [12, 18]
 
 read_attrs = [(mx28.ADDR_PRESENT_POSITION, mx28.LEN_PRESENT_POSITION),
               (mx28.ADDR_PRESENT_SPEED, mx28.LEN_PRESENT_SPEED)]
@@ -50,68 +50,75 @@ def initialize_port():
 
 def read_state(port_num):
 
-    bulk_read_packet = dynamixel.groupBulkRead(port_num, PROTOCOL_VERSION)
+    # bulk_read_packet = dynamixel.groupBulkRead(port_num, PROTOCOL_VERSION)
 
     # Add parameters to the bulk packetHandler
 
-    for id, attr in read_list:
-        add_result = ctypes.c_ubyte(dynamixel.groupBulkReadAddParam(
-            bulk_read_packet, id, attr[0], attr[1])).value
-        if add_result != 1:
-            raise RuntimeError("Failed to add storage for motor " + str(id)
-                               + " address " + attr[0])
+    # for id, attr in read_list:
+    #     add_result = ctypes.c_ubyte(dynamixel.groupBulkReadAddParam(
+    #         bulk_read_packet, id, attr[0], attr[1])).value
+    #     if add_result != 1:
+    #         raise RuntimeError("Failed to add storage for motor " + str(id)
+    #                            + " address " + attr[0])
 
-    dynamixel.groupBulkReadTxRxPacket(bulk_read_packet)
-    check_comm_error(port_num)
+    # dynamixel.groupBulkReadTxRxPacket(bulk_read_packet)
+    # check_comm_error(port_num)
 
     results = [None] * len(read_list)
 
-    for index, (id, attr) in read_list:
-        getdata_result = ctypes.c_ubyte(
-            dynamixel.groupBulkReadIsAvailable(bulk_read_packet, id,
-                                               attr[0], attr[1])).value
-        if getdata_result != 1:
-            raise RuntimeWarning("Failed reading motor " + str(id),
-                                 + " address " + attr[0])
-        else:
-            results[index] = dynamixel.groupBulkReadGetData(bulk_read_packet,
-                                                            id, attr[0],
-                                                            attr[1])
+    for index, (id, attr) in enumerate(read_list):
+        # getdata_result = ctypes.c_ubyte(
+        #     dynamixel.groupBulkReadIsAvailable(bulk_read_packet, id,
+        #                                        attr[0], attr[1])).value
+        # if getdata_result != 1:
+        #     raise RuntimeWarning("Failed reading motor " + str(id),
+        #                          + " address " + attr[0])
+        # else:
+        #     results[index] = dynamixel.groupBulkReadGetData(bulk_read_packet,
+        #                                                     id, attr[0],
+        #                                                     attr[1])
 
+        if attr[1] == 1:
+            val = dynamixel.read1ByteTxRx(port_num, PROTOCOL_VERSION, id, attr[0])
+        elif attr[1] == 2:
+            val = dynamixel.read2ByteTxRx(port_num, PROTOCOL_VERSION, id, attr[0])
+        elif attr[2] == 4:
+            val = dynamixel.read4ByteTxRx(port_num, PROTOCOL_VERSION, id, attr[0])
+        else:
+            raise RuntimeError("Invalid data size")
+        check_comm_error(port_num)
+        results[index] = val
     return results
 
 def write_positions(port_num, targets):
 
-    bulk_write_packet = dynamixel.groupBulkWrite(port_num, PROTOCOL_VERSION)
+    # bulk_write_packet = dynamixel.groupBulkWrite(port_num, PROTOCOL_VERSION)
 
     for index, (id, attr) in enumerate(write_list):
-        add_result = ctypes.c_ubyte(
-            dynamixel.groupBulkWriteAddParam(bulk_write_packet, id,
-                                             attr[0],
-                                             attr[1],
-                                             targets[index], attr[1])).value
-        if add_result != 1:
-            raise RuntimeError("Failed to add instruction for motor " + str(id))
+        # add_result = ctypes.c_ubyte(
+        #     dynamixel.groupBulkWriteAddParam(bulk_write_packet, id,
+        #                                      attr[0],
+        #                                      attr[1],
+        #                                      targets[index], attr[1])).value
+        # if add_result != 1:
+        #     raise RuntimeError("Failed to add instruction for motor " + str(id))
+        if attr[1] == 1:
+            dynamixel.write1ByteTxRx(port_num, PROTOCOL_VERSION, id, attr[0], targets[index])
+        elif attr[1] == 2:
+            dynamixel.write2ByteTxRx(port_num, PROTOCOL_VERSION, id, attr[0], targets[index])
+        elif attr[2] == 4:
+            dynamixel.write4ByteTxRx(port_num, PROTOCOL_VERSION, id, attr[0], targets[index])
+        else:
+            raise RuntimeError("Invalid data size")
+        check_comm_error(port_num)
 
-    dynamixel.groupBulkWriteTxPacket(bulk_write_packet)
-    check_comm_error(port_num)
+    # dynamixel.groupBulkWriteTxPacket(bulk_write_packet)
+    # check_comm_error(port_num)
 
     # Clear bulkwrite parameter storage
-    dynamixel.groupBulkWriteClearParam(bulk_write_packet)
+    # dynamixel.groupBulkWriteClearParam(bulk_write_packet)
 
 if __name__ == "__main__":
     port_num, phandler = initialize_port()
-
-
-    dxl_model_number = dynamixel.pingGetModelNum(port_num, PROTOCOL_VERSION, dxl_ids[0])
-    dxl_comm_result = dynamixel.getLastTxRxResult(port_num, PROTOCOL_VERSION)
-    dxl_error = dynamixel.getLastRxPacketError(port_num, PROTOCOL_VERSION)
-    if dxl_comm_result != COMM_SUCCESS:
-        print(dynamixel.getTxRxResult(PROTOCOL_VERSION, dxl_comm_result))
-    elif dxl_error != 0:
-        print(dynamixel.getRxPacketError(PROTOCOL_VERSION, dxl_error))
-    else:
-        print("ping Succeeded on " + str(dxl_ids[0]))
-
-    for i in range(5):
-        read_state(port_num)
+    print(read_state(port_num))
+    write_positions(port_num, [0, 700])
