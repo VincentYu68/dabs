c97d7d28eed6903a05a61935ca17d50f1646a635
Wenhao Yu <wenhaoyu@Wenhaos-MacBook-Pro.local>
2018-12-09 20:52:46 -0500

    add up policy

---
+++ darwin/run_socma.py
@@ -0,0 +1,90 @@
+# optimize the strategy using cma
+
+import numpy as np
+from darwin.darwin_utils import *
+from darwin.basic_darwin import *
+from darwin.np_policy import *
+import joblib
+from dabs import *
+import time
+import os, errno
+from darwin.strategy_optimizer import *
+
+if __name__ == "__main__":
+    filename = 'sqstsqst_up.pkl'
+
+    walk_motion = False
+    singlefoot_motion = False
+    crawl_motion = False
+
+    gyro_input = 0
+
+    pose_squat_val = np.array([2509, 2297, 1714, 1508, 1816, 2376,
+                               2047, 2171,
+                               2032, 2039, 2795, 648, 1231, 2040, 2041, 2060, 1281, 3448, 2855, 2073])
+    pose_stand_val = np.array([1500, 2048, 2048, 2500, 2048, 2048,
+                               2048, 2048,
+                               2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048, 2048])
+
+    pose_squat = VAL2RADIAN(pose_squat_val)
+    pose_stand = VAL2RADIAN(pose_stand_val)
+
+    # keyframe scheduling for squat stand task
+    interp_sch = [[0.0, pose_stand],
+                           [2.0, pose_squat],
+                           [4.0, pose_stand],
+                           [5.0, pose_stand],
+                           [5.5, pose_squat],
+                           [6.0, pose_stand],
+                           [8.0, pose_stand],
+                           ]
+
+    if walk_motion or crawl_motion:
+        if walk_motion:
+            rig_keyframe = np.loadtxt('data/rig_data/rig_keyframe.txt')
+        else:
+            rig_keyframe = np.loadtxt('data/rig_data/rig_keyframe_crawl.txt')
+        interp_sch = [[0.0, rig_keyframe[0]]]
+        interp_time = 0.5
+        for i in range(10):
+            for k in range(1, len(rig_keyframe)):
+                interp_sch.append([interp_time, rig_keyframe[k]])
+                interp_time += 0.5
+        interp_sch.append([interp_time, rig_keyframe[0]])
+
+    if singlefoot_motion:
+        rig_keyframe = np.loadtxt('data/rig_data/rig_keyframe2.txt')
+        interp_sch = [[0.0, rig_keyframe[0]],
+                      [2.0, rig_keyframe[1]],
+                      [6.0, rig_keyframe[1]]]
+
+    policy = NP_Policy(interp_sch, 'data/'+filename, discrete_action=True,
+                       action_bins=np.array([11] * 20), delta_angle_scale=0.3, action_filter_size=5)
+
+    darwin = BasicDarwin()
+
+    darwin.connect()
+
+    darwin.write_torque_enable(True)
+
+    darwin.write_pid(32, 0, 16)
+
+    optimizer = StrategyOptimizer(darwin, policy, 3, 2)
+
+    optimizer.optimize()
+
+    darwin.disconnect()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ darwin/strategy_optimizer.py
@@ -0,0 +1,84 @@
+import cma, sys, gym, joblib
+import numpy as np
+from darwin.darwin_utils import *
+import time
+
+class StrategyOptimizer:
+    def __init__(self, robot, policy, strategy_dim, eval_num = 2):
+        self.robot = robot
+        self.policy = policy
+        self.strategy_dim = strategy_dim
+        self.eval_num = eval_num
+        self.sample_num = 0
+        self.rollout_num = 0
+
+        self.solution_history = []
+        self.sample_num_history = []
+        self.best_f = 100000
+        self.best_x = None
+
+    def reset(self):
+        self.sample_num = 0
+        self.rollout_num = 0
+
+        self.best_f = 100000
+        self.best_x = None
+        self.solution_history = []
+        self.sample_num_history = []
+
+    def fitness(self, x):
+        app = np.copy(x)
+        avg_perf = []
+        print("Start a new trial with parameter: ", x)
+        for _ in range(self.eval_num):
+            dummpy_input = input("press any key to start trial " + str(_))
+
+            self.robot.write_motor_goal(RADIAN2VAL(SIM2HW_INDEX(self.policy.get_initial_state())))
+
+            time.sleep(5)
+
+            prev_motor_pose = np.array(self.robot.read_motor_positions())
+            current_step = 0
+            initial_time = time.monotonic()
+            prev_time = time.monotonic()
+            while current_step < 200:
+                if time.monotonic() - prev_time >= 0.05:  # control every 50 ms
+                    # tdif = time.monotonic() - prev_time
+                    prev_time = time.monotonic() - ((time.monotonic() - prev_time) - 0.05)
+                    motor_pose = np.array(self.robot.read_motor_positions())
+                    obs_input = VAL2RADIAN(np.concatenate([HW2SIM_INDEX(prev_motor_pose), HW2SIM_INDEX(motor_pose)]))
+                    ct = time.monotonic() - initial_time
+                    act = self.policy.act(np.concatenate([obs_input, app]), ct)
+                    self.robot.write_motor_goal(RADIAN2VAL(SIM2HW_INDEX(act)))
+
+                    prev_motor_pose = np.copy(motor_pose)
+
+                    current_step += 1
+            rollout_rew = float(input("Input the estimated reward for this rollout"))
+            self.rollout_num += 1
+            avg_perf.append(rollout_rew)
+
+        if -np.mean(avg_perf) < self.best_f:
+            self.best_x = np.copy(x)
+            self.best_f = -np.mean(avg_perf)
+        print('Sampled perf: ', np.mean(avg_perf))
+        return -np.mean(avg_perf)
+
+    def cames_callback(self, es):
+        self.solution_history.append(self.best_x)
+        self.sample_num_history.append(self.sample_num)
+        return self.sample_num
+
+
+    def optimize(self, maxiter = 20):
+        init_guess = np.random.random(self.strategy_dim)
+        init_std = 0.5
+        bound = [0.0, 1.0]
+
+        xopt, es = cma.fmin2(self.fitness, init_guess, init_std, options={'bounds': bound, 'maxiter': maxiter}
+                             , callback=self.cames_callback)
+
+        print('optimized: ', repr(xopt))
+        print('Total rollout: ', self.rollout_num)
+
+        return xopt
