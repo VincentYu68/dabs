c511f3dfc7fa9e6f0bf41573ba1ea735730b6bfa
Wenhao Yu <wenhaoyu@Wenhaos-MacBook-Pro.local>
2019-01-16 11:52:54 -0500

    add test imu

--- darwin/test_imu.py
+++ darwin/test_imu.py
@@ -1,6 +1,23 @@
+from darwin.basic_darwin import *
 from darwin.darwin_utils import *
+import numpy as np
 from bno055_usb_stick_py import BnoUsbStick
 bno_usb_stick = BnoUsbStick()
 bno_usb_stick.activate_streaming()
-for packet in bno_usb_stick.recv_streaming_generator():
-    print(DEGREE2RAD(packet.euler))
+
+
+if __name__ == '__main__':
+    darwin = BasicDarwin()
+
+    darwin.connect()
+
+    darwin.write_torque_enable(True)
+    darwin.write_pid(32, 0, 16)
+
+    motor_pose = darwin.read_motor_positions()
+
+    darwin.write_motor_goal(np.zeros(20))
+
+
+    for packet in bno_usb_stick.recv_streaming_generator():
+        print(DEGREE2RAD(packet.euler))
