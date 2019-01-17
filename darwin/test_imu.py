6afac5b5c9dee91b120df6a33d84fe7b5a3168d6
Wenhao Yu <wenhaoyu@Wenhaos-MacBook-Pro.local>
2019-01-17 17:11:06 -0500

    add imu test

--- darwin/test_imu.py
+++ darwin/test_imu.py
@@ -2,11 +2,20 @@ from darwin.basic_darwin import *
 from darwin.darwin_utils import *
 import numpy as np
 from bno055_usb_stick_py import BnoUsbStick
-bno_usb_stick = BnoUsbStick()
-bno_usb_stick.activate_streaming()
+
 
 
 if __name__ == '__main__':
+    poses_to_test = []
+    poses_to_test.append(RADIAN2VAL(np.zeros(20)))
+    poses_to_test.append(SIM2HW_INDEX(0.5*(pose_squat_val + pose_stand_val)))
+    poses_to_test.append(SIM2HW_INDEX(pose_squat_val))
+
+    bno_usb_stick = BnoUsbStick()
+    bno_usb_stick.activate_streaming()
+
+    pose_id = 0
+
     darwin = BasicDarwin()
 
     darwin.connect()
@@ -16,8 +25,15 @@ if __name__ == '__main__':
 
     motor_pose = darwin.read_motor_positions()
 
-    darwin.write_motor_goal(RADIAN2VAL(np.zeros(20)))
+    darwin.write_motor_goal(SIM2HW_INDEX(pose_squat_val))
 
-
-    for packet in bno_usb_stick.recv_streaming_generator():
-        print(DEGREE2RAD(np.array(packet.euler)), DEGREE2RAD(np.array(packet.g)))
+    while True:
+        cmd = input('Input command. n: next pose, g: print current gyro reading, c: print gyro reading for 100 steps')
+        if cmd == 'n':
+            darwin.write_motor_goal(poses_to_test[pose_id])
+            pose_id += 1
+        if cmd == 'g':
+            print(darwin.read_bno055_gyro())
+        if cmd == 'c':
+            for i in range(100):
+                print(darwin.read_bno055_gyro())
