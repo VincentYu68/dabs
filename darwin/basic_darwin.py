43c7b68fadee102f9241c0096f058a665895dc4b
Wenhao Yu <wenhaoyu@Wenhaos-MacBook-Pro.local>
2019-01-17 17:36:27 -0500

    add imu test

--- darwin/basic_darwin.py
+++ darwin/basic_darwin.py
@@ -78,7 +78,7 @@ class BasicDarwin:
         if self.use_bno055:
             from bno055_usb_stick_py import BnoUsbStick
             self.bno_usb_stick = BnoUsbStick()
-            #self.bno_usb_stick.activate_streaming()
+            self.bno_usb_stick.activate_streaming()
 
     def disconnect(self):
         self.port_handler.closePort()
@@ -88,19 +88,20 @@ class BasicDarwin:
 
         reg_start_addr = 0X14
         num_registers = 0x0C
-        burst_read_result = self.bno_usb_stick.burst_read(reg_start_addr, num_registers)
-        print(burst_read_result[0], burst_read_result[1])
-        euler1 = DEGREE2RAD(np.array(burst_read_result[0])[3:] / 16.)
-        angvel1 = DEGREE2RAD(np.array(burst_read_result[0])[0:3] / 16.)
-        print(euler1, angvel1)
-        '''packet = self.bno_usb_stick.recv_streaming_packet()
+        #burst_read_result = self.bno_usb_stick.burst_read(reg_start_addr, num_registers)
+        #print(burst_read_result[0], burst_read_result[1])
+        #euler1 = DEGREE2RAD(np.array(burst_read_result[0])[3:] / 16.)
+        #angvel1 = DEGREE2RAD(np.array(burst_read_result[0])[0:3] / 16.)
+        #print(euler1, angvel1)
+        packet = self.bno_usb_stick.recv_streaming_packet()
 
         euler = DEGREE2RAD(np.array(packet.euler))
         angvel = DEGREE2RAD(np.array(packet.g))
+        print(euler, angvel)
         
         if euler[0] > np.pi:
             euler[0] -= 2 * np.pi
-        gyro_data.append(np.array([-euler[1], euler[2], -euler[0],   angvel[1], -angvel[0], angvel[2]]))'''
+        gyro_data.append(np.array([-euler[1], euler[2], -euler[0],   angvel[1], -angvel[0], angvel[2]]))
         return np.mean(gyro_data, axis=0)
 
 
